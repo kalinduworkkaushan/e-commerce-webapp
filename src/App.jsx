945062766a5dd428533e5c6ca5286418b8633aa9
it21044618 <it21044618@my.sliit.lk>
2026-07-01 15:37:52 +0530

    The initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Home from "./Components/Home"
+import Footer from "./Components/Footer"
+import Navbar from "./Components/Navbar"
+import Login from "./Components/Login/Login"
+
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+    <nav>
+      <Link to="/home">Home</Link>
+    </nav>
+    <Routes>
+      <Route path="/" element={<Login/>}/>
+      <Route path="/navbar" element={<Navbar/>} />
+      <Route path="/home" element={<Home/>} />
+     </Routes>
+     </BrowserRouter>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer(){
+    return(
+        <>
+            <p>Copyright © 2023 My App. All rights reserved.</p>
+        </>
+    )
+}
